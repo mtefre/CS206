aab7f614a15e1d0c55e0c8f3aeb295f73aec018a
Mathias Tefre <tefremathias@gmail.com>
2022-01-18 21:32:16 -0500

    loop 100.

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time 
+i = 0
+
 physicsClient = p.connect(p.GUI)
+
+for x in range(1000):
+    p.stepSimulation()
+    time.sleep(1/200)
+    i += 1
+    print(i)
+    
 p.disconnect()
- 
