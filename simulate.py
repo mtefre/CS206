45213f37c15c762d38f00fce49eb1382a4da0fb0
Mathias Tefre <tefremathias@gmail.com>
2022-01-25 16:43:57 -0500

    Gravity

--- simulate.py
+++ simulate.py
@@ -3,10 +3,11 @@ import time
 i = 0
 
 physicsClient = p.connect(p.GUI)
-
+p.setGravity(0,0,-9.9)
+p.loadSDF("box.sdf")
 for x in range(1000):
     p.stepSimulation()
-    time.sleep(1/200)
+    time.sleep(1/50)
     i += 1
     print(i)
     
