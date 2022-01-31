5d4e2f66df1a263eea70d42ed00d2deba89df6ad
Mathias Tefre <tefremathias@gmail.com>
2022-01-30 19:28:47 -0500

    rob + legs

--- generate.py
+++ generate.py
@@ -8,36 +8,17 @@ y = 0
 z = 0.5
 
 
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+  
+    pyrosim.End()
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name = "Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,0.5])
+    pyrosim.Send_Cube(name = "BackLeg", pos=[0.5,0,1] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "BackLeg_FrontLeg" , parent= "BackLeg" , child = "FrontLeg" , type = "revolute", position = [0.5,0,0])
+    pyrosim.Send_Cube(name = "FrontLeg", pos=[1,0,0] , size=[1,1,1])
 
-for y in range(10):
-    y = -1
-    l-=0.05
-    w-=0.05
-    h-=0.05
-    for j in range(5):
-        x=-1.1
-        y+=1.1
-        z+=1.1
-        for i in range(5):
-            x+=1.1
-            pyrosim.Send_Cube(name = "Box", pos=[x,y,z] , size=[l,w,h])
-                      
-            
-
-           
-        
-        
-
-        
-            
-
-           
-    
-       
-        
-            
-
-pyrosim.End()
-
+    pyrosim.End()
