bdb9894ae5f70bd42e8438f1646dfe27651fd342
Mathias Tefre <tefremathias@gmail.com>
2022-02-07 16:06:40 -0500

    updated sensors

--- generate.py
+++ generate.py
@@ -1,25 +1,19 @@
 import pyrosim.pyrosim as pyrosim 
 
-l = 1
-w = 1
-h = 1
-x = 0
-y = 0
-z = 0.5
 
-
-def Create_World():
+def create_world():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="box", pos=[1, 3, 0.5], size=[1, 1, 1])
     pyrosim.End()
 
-def Create_Robot():
-    pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name = "Torso", pos=[0,0,0.5] , size=[1,1,1])
-    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,0.5])
-    pyrosim.Send_Cube(name = "BackLeg", pos=[0.5,0,1] , size=[1,1,1])
-    pyrosim.Send_Joint( name = "BackLeg_FrontLeg" , parent= "BackLeg" , child = "FrontLeg" , type = "revolute", position = [0.5,0,0])
-    pyrosim.Send_Cube(name = "FrontLeg", pos=[1,0,0] , size=[1,1,1])
 
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0.5, 0, 1], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="Torso", child="BackLeg", type="revolute", position=[0.5, 0, 0.5])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, 0])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[1, 0, 0], size=[1, 1, 1])
 
     pyrosim.End()
+
--- simulate.py
+++ simulate.py
@@ -11,14 +11,14 @@ p.setGravity(0, 0, -9.9)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 worldId = p.loadSDF("world.sdf")
-
 pyrosim.Prepare_To_Simulate(robotId)
 
 for x in range(50000):
-    Create_Robot()
+    create_robot()
     p.stepSimulation()
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     print(backLegTouch)
+    create_world()
     time.sleep(1 / 60)
 
 p.disconnect()
