cff04eac8e55fed82533b2348d9c4e19086a2fe7
Mathias Tefre <tefremathias@gmail.com>
2022-02-07 16:11:26 -0500

    updated sensors

--- generate.py
+++ generate.py
@@ -12,6 +12,8 @@ def create_robot():
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0.5, 0, 0.5])
     pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, 1], size=[1, 1, 1])
-    pyrosim.Send_Joint(name="BackLeg_FrontLeg", parent="BackLeg", child="FrontLeg", type="revolute", position=[0.5, 0, 0])
+    pyrosim.Send_Joint(name="BackLeg_FrontLeg", parent="BackLeg", child="FrontLeg", type="revolute",
+                       position=[0.5, 0, 0])
     pyrosim.Send_Cube(name="FrontLeg", pos=[1, 0, 0], size=[1, 1, 1])
+
     pyrosim.End()
