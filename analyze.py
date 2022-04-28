f9be3933cae3f58e53e46e648ad911b3c22fcc58
Mathias Tefre <tefremathias@gmail.com>
2022-04-28 00:27:59 -0400

    updated files

--- analyze.py
+++ analyze.py
@@ -1,9 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
 
-time = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-value = (0.10,0.55,0.99,1.1,1.2,1.1,1.11,1.51,1.641, 2.151, 2.24, 2.24, 2.24, 2.24, 2.24, 2.24, 2.24, 2.24, 2.24,2.24)
-
 backLegSensorValues = numpy.load('data/BestFitnessA.npy')
 
 
