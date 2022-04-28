6bad317f05891b1fb2eeecd21798d22e3f57569a
Mathias Tefre <tefremathias@gmail.com>
2022-04-28 00:29:28 -0400

    updated files

--- analyze.py
+++ analyze.py
@@ -5,11 +5,11 @@ time = (0, 10, 20, 30, 40)
 data = (1.3, 1.5, 2.4, 3.1, 3.3)
 
 RobotA = numpy.load('data/BestFitnessA.npy')
-RobotB = numpy.load('data2/BestFitnessB.npy')
+# RobotB = numpy.load('data2/BestFitnessB.npy')
 
 
 plt.plot(RobotA, label="RobotA", linewidth=2.8)
-plt.plot(RobotB, label="RobotB", linewidth=2.8)
+# plt.plot(RobotB, label="RobotB", linewidth=2.8)
 
 plt.legend(loc='upper right')
 plt.show()
